6f37dcf81dbe6bd4c5765c5f9d6102dc60a7de6b
Duilio Restuccia <restucciaduilio@gmail.com>
2022-09-15 19:16:48 -0500

    Add files via upload

---
+++ db.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine, MetaData
+
+#engine = create_engine("mysql+pymysql://root:Feifei3619/@127.0.0.1:3306/vintec_m3")
+engine = create_engine("mysql+pymysql://root:Feifei3619/@127.0.0.1:3306/racesf1")
+#conn = engine.connect() no usar
+
+meta_data = MetaData()
